84b956f11670eab7bc039e415fa04a468db8735f
M Anil Kamath <anilkamath27@gmail.com>
2023-11-02 10:01:01 +0530

    update

--- OIBSIP-Internship-Car-Price-Prediction/steps/config.py
+++
@@ -1,7 +0,0 @@
-from zenml.steps import BaseParameters
-
-class ModelNameConfig(BaseParameters):
-    """Model Configurations"""
-    model_name:str = "RandomForestRegressor"
-
-    # Replace RandomForestRegressor with the model name of your choice to run model. 
